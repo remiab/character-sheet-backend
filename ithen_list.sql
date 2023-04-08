837e7f1fdc187fdb7c44a585013bafbca6e12230
remiab <remi.bevan@gmail.com>
2023-04-08 22:48:33 +0100

    created stored procedure to view ithen's spells

---
+++ ithen_list.sql
@@ -0,0 +1,15 @@
+USE ttrpg;
+
+-- CREATE TABLE ithen_spell_list AS
+-- SELECT spell_name FROM spells;
+
+-- ALTER TABLE ithen_spell_list
+-- ADD COLUMN prepared VARCHAR(3);
+
+CALL view_ithen_spells();
+
+
+
+
+
+-- SELECT * FROM ithen_spell_list;
